94eec705b90a994956b2ff21bdeb08678e37d741
jhonf16071990 <jfsuarezg@hotmail.com>
2025-06-26 05:52:15 -0500

    Add .next/static/chunks/src_app_resultados_page_tsx_0ef0cac3._.js 1750935137705

---
+++ .next/static/chunks/src_app_resultados_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_42e647bc._.js",
+  "static/chunks/src_dbd2de89._.js"
+],
+    source: "dynamic"
+});
